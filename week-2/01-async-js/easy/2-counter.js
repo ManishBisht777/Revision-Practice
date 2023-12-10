ef5a5c310a055996a2d548bea89f11d25f228ea6
ManishBisht7 <manishbisht9711@gmail.com>
2023-12-10 16:47:49 +0530

    feat: better way?

--- week-2/01-async-js/easy/2-counter.js
+++ week-2/01-async-js/easy/2-counter.js
@@ -13,3 +13,22 @@ function PrintCounterValue(counter) {
 }
 
 CounterWithoutSetInterval(5, PrintCounterValue);
+
+// 2nd solution
+// better way? ig yes
+function delay(seconds) {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+}
+
+async function counterWithoutSetInterval(timeInSeconds, callback) {
+  for (let i = 1; i <= timeInSeconds; i++) {
+    await delay(1);
+    callback(i);
+  }
+}
+
+function printCounterValue(counter) {
+  console.log(counter);
+}
+
+counterWithoutSetInterval(5, printCounterValue);
